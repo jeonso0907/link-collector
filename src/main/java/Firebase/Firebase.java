c1dab9191dd4f4a7469f966e94a6e2584d7f94a5
Sooyoung Jeon <sjeon@lexon-corp.com>
2021-06-28 16:37:14 -0400

    develop/backend
    Restructured the file organization

--- src/main/java/Firebase/Firebase.java
+++ src/main/java/Firebase/Firebase.java
@@ -18,7 +18,7 @@ public class Firebase {
 
 
         FileInputStream serviceAccount =
-                new FileInputStream("/Users/sooyoung/Desktop/link-collector-41e61-firebase-adminsdk-ilih0-6179a8c8ef.json");
+                new FileInputStream("src/main/resources/FirestoreKey/firestore.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -1,17 +1,11 @@
 package Model;
 
-import Controller.LinkController;
-import Controller.LoginController;
 import Firebase.Firebase;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class Model extends Application{
 
